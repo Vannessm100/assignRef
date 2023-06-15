a8b6615d9026af3aabd517d551f82c7c5500a25b
Vannessm100 <125915179+Vannessm100@users.noreply.github.com>
2023-06-15 17:40:18 -0400

    Add files via upload

---
+++ Net code/ZoomApiController.cs
@@ -0,0 +1,314 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Sabio.Models.AppSettings;
+using Sabio.Models.Domain.Zoom;
+using Sabio.Models.Requests.Zoom;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace Sabio.Web.Api.Controllers
+{
+
+
+
+    namespace Sabio.Web.Api.Controllers
+    {
+        [Route("api/zoom")]
+        [ApiController]
+        public class ZoomApiController : BaseApiController
+        {
+            public IZoomService _service { get; set; }
+         
+
+            private IAuthenticationService<int> _authService = null;
+            private ZoomConfig _zoomConfig;
+
+            public ZoomApiController(IZoomService service, ILogger<ZoomApiController> logger, IOptions<ZoomConfig> zoomConfig, IAuthenticationService<int> authService) : base(logger)
+            {
+
+                _service = service;
+                _authService = authService;
+                _zoomConfig = zoomConfig.Value;
+                var clientSecret = _zoomConfig.ClientSecret;
+                var clientId = _zoomConfig.ClientId;
+            }
+
+
+
+            [HttpGet("authenticate")]
+            public ActionResult<string> GetAuth()
+            {
+                int code = 302;
+                BaseResponse response = null;
+
+                try
+                {
+                    string authUrl = _service.GetAuthenticationCode();
+
+                    if (authUrl == null)
+                    {
+                        code = 404;
+                        response = new ErrorResponse("Improper Authorization");
+                    }
+                    else
+                    {
+
+                        return authUrl;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    code = 500;
+                    response = new ErrorResponse(ex.Message);
+                    base.Logger.LogError(ex.ToString());
+                }
+
+                return StatusCode(code, response);
+
+
+            }
+
+            [HttpGet("token")]
+            public async Task<ActionResult<ItemResponse<string>>> GetAuthToken(string code)
+            {
+                int responseCode = 200;
+                BaseResponse response = null;
+
+                try
+                {
+                    string accessToken = await _service.GetToken(code);
+                    string hasToken = "true";
+
+                    if (accessToken == null)
+                    {
+                        responseCode = 404;
+                        response = new ErrorResponse("No token");
+                    }
+                    else
+                    {
+
+                        var accessTokenCookieOptions = new CookieOptions
+                        {
+
+                            Secure = true,
+                            MaxAge = TimeSpan.FromMinutes(60),
+                            HttpOnly = true,
+                            SameSite = SameSiteMode.None,
+                            IsEssential = true,
+
+                        };
+                        var hasTokenCookieOptions = new CookieOptions
+                        {
+
+                            Secure = true,
+                            MaxAge = TimeSpan.FromMinutes(60),
+
+                        };
+
+                        Response.Cookies.Append("accessToken", accessToken, accessTokenCookieOptions);
+                        Response.Cookies.Append("hasToken", hasToken, hasTokenCookieOptions);
+                        response = new ItemResponse<string> { Item = accessToken };
+                        //link for testing purposes
+                        return Redirect(_zoomConfig.ReturnUrl);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    responseCode = 500;
+                    response = new ErrorResponse(ex.Message);
+                    base.Logger.LogError(ex.ToString());
+                }
+                return StatusCode(responseCode, response);
+
+            }
+
+
+            [HttpGet("user")]
+            public async Task<ActionResult<ItemResponse<ZoomUser>>> GetCurrentUser()
+
+            {
+                int responseCode = 200;
+                BaseResponse response = null;
+
+                try
+                {
+
+
+                    string accessToken = Request.Cookies["accessToken"];
+                    ZoomUser user = await _service.GetUser(accessToken);
+
+                    if (accessToken == null || accessToken.Length < 1)
+                    {
+                        responseCode = 404;
+                        response = new ErrorResponse("No token");
+
+                    }
+                    else
+                    {
+                        response = new ItemResponse<ZoomUser> { Item = user };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    responseCode = 500;
+                    response = new ErrorResponse(ex.Message);
+                    base.Logger.LogError(ex.ToString());
+                }
+
+                return StatusCode(responseCode, response);
+            }
+
+            [HttpPost("meeting")]
+            public async Task<ActionResult<ItemResponse<ZoomMeeting>>> CreateMeeting(ZoomMeetingRequest model)
+            {
+
+                ZoomMeetingAddRequest request = new ZoomMeetingAddRequest();
+
+                request.topic = model.Topic;
+                request.start_time = model.StartTime;
+                request.type = model.Type;
+                request.duration = model.Duration;
+                request.timezone = model.Timezone;
+                request.hostvideo = model.HostVideo;
+                request.participantvideo = model.ParticipantVideo;
+
+
+
+                int responseCode = 200;
+                BaseResponse response = null;
+
+                try
+                {
+
+
+                    string accessToken = Request.Cookies["accessToken"];
+                    ZoomMeeting meeting = await _service.CreateZoomMeeting(accessToken, request);
+
+
+                    if (accessToken == null || accessToken.Length < 1)
+                    {
+
+                        responseCode = 401;
+                    }
+                    else
+                    {
+                        response = new ItemResponse<ZoomMeeting> { Item = meeting };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    responseCode = 500;
+                    response = new ErrorResponse(ex.Message);
+                    base.Logger.LogError(ex.ToString());
+                }
+
+                return StatusCode(responseCode, response);
+            }
+            [HttpPost("saveMeeting")]
+            public ActionResult<ItemResponse<int>> SaveMeeting(BaseMeeting model)
+            {
+                ObjectResult result = null;
+
+                try
+                {
+                    int id = _service.Add(model);
+
+                    ItemResponse<int> response = new ItemResponse<int>();
+
+                    response.Item = id;
+
+
+                    result = Created201(response);
+
+                }
+                catch (Exception ex)
+                {
+
+                    Logger.LogError(ex.ToString());
+                    ErrorResponse response = new ErrorResponse(ex.Message);
+
+                    result = StatusCode(500, response);
+                }
+                return result;
+            }
+
+            [HttpGet("getUserMeetings/{id:int}")]
+            public ActionResult<ItemResponse<UserMeetingRequest>> GetById(int id)
+            {
+                int code = 200;
+                BaseResponse response = null;
+
+                try
+                {
+                    List<UserMeetingRequest> meetings = _service.GetById(id);
+
+                    if (meetings == null)
+                    {
+                        code = 404;
+                        response = new ErrorResponse("Application Resource not found.");
+
+                    }
+                    else
+                    {
+                        response = new ItemsResponse<UserMeetingRequest> { Items = meetings };
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    code = 500;
+                    Logger.LogError(ex.ToString());
+                    response = new ErrorResponse($"Generic Error: {ex.Message}");
+                }
+
+                return StatusCode(code, response);
+            }
+
+            [HttpGet("pastMeetings/{meetingId:long}")]
+            public async Task<ActionResult<ItemResponse<MeetingInformationRequest>>> GetZoomMeeting(long meetingId)
+
+            {
+                int responseCode = 200;
+                BaseResponse response = null;
+
+                try
+                {
+
+
+                    string accessToken = Request.Cookies["accessToken"];
+                    MeetingInformationRequest meeting = await _service.GetMeeting(accessToken, meetingId);
+
+                    if (accessToken == null || accessToken.Length < 1)
+                    {
+                        responseCode = 404;
+                        response = new ErrorResponse("No token");
+
+                    }
+                    else
+                    {
+                        response = new ItemResponse<MeetingInformationRequest> { Item = meeting };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    responseCode = 500;
+                    response = new ErrorResponse(ex.Message);
+                    base.Logger.LogError(ex.ToString());
+                }
+
+                return StatusCode(responseCode, response);
+            }
+
+
+        }
+    }
+}
---
+++ Net code/ZoomService.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Portable;
+using Sabio.Data.Providers;
+using Sabio.Models.AppSettings;
+using Sabio.Models.Domain.Zoom;
+using Sabio.Models.Requests.Zoom;
+using Sabio.Services.Interfaces;
+using IRestClient = RestSharp.IRestClient;
+using Method = RestSharp.Method;
+using ParameterType = RestSharp.ParameterType;
+using RestRequest = RestSharp.RestRequest;
+using RestResponse = RestSharp.RestResponse;
+using System.Data;
+using Sabio.Data;
+
+
+namespace Sabio.Services
+{
+
+
+    public class ZoomService : IZoomService
+    {
+        private IRestClient _restClient;
+        private ZoomConfig _zoomConfig;
+        IDataProvider _data = null;
+
+
+
+        public ZoomService(IRestClient restClient, IOptions<ZoomConfig> zoomConfig, IDataProvider data)
+        {
+            _restClient = restClient;
+            _zoomConfig = zoomConfig.Value;
+            _data = data;
+
+        }
+
+
+        public string GetAuthenticationCode()
+        {
+         
+            var clientId = _zoomConfig.ClientId;
+            var redirectUri = _zoomConfig.RedirectUrl;
+            var authorizationUrl = $"https://zoom.us/oauth/authorize?response_type=code&client_id={clientId}&redirect_uri={redirectUri}";
+            return authorizationUrl;
+        }
+
+        public async Task<string> GetToken([FromBody] string code)
+        {
+            string authorizationCode = GetAuthenticationCode();
+            var clientSecret = _zoomConfig.ClientSecret;
+            var clientId = _zoomConfig.ClientId;
+
+            var clientSecretId = $"{clientId}:{clientSecret}";
+            byte[] byt = Encoding.UTF8.GetBytes(clientSecretId);
+            var encodedId = Convert.ToBase64String(byt);
+            var redirectUri = _zoomConfig.RedirectUrl;
+            var request = new RestRequest("https://zoom.us/oauth/token", Method.Post);
+            request.AddHeader("Authorization", "Basic " + $"{encodedId}");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("code", code, ParameterType.GetOrPost );
+            request.AddParameter("grant_type", "authorization_code", ParameterType.GetOrPost);
+            request.AddParameter("redirect_uri", redirectUri, ParameterType.GetOrPost);
+            var response = await _restClient.ExecuteAsync<ZoomTokenResponse>(request);
+
+            if (response.IsSuccessful == false)
+            {
+                throw new Exception($"Failed to retrieve access token: {response.ErrorMessage}");
+            }
+
+            var jsonResponse = JObject.Parse(response.Content);
+            var accessToken = jsonResponse["access_token"].Value<string>();
+
+            return accessToken;
+        }
+
+        public async Task<ZoomUser> GetUser(string accessToken) 
+        {
+
+            if (accessToken != null)
+            {
+                var options = new RestClientOptions("https://api.zoom.us")
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+                var request = new RestRequest("/v2/users/me?login_type=1&encrypted_email=false&search_by_unique_id=<boolean>", Method.Get);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", $"Bearer {accessToken}");
+                RestResponse response = await client.ExecuteAsync(request);
+                JObject jsonResponse = JObject.Parse(response.Content);
+                var user = new ZoomUser
+                {
+                    Id = jsonResponse["id"].Value<string>(),
+                    FirstName = jsonResponse["first_name"].Value<string>(),
+                    LastName = jsonResponse["last_name"].Value<string>(),
+                    Email = jsonResponse["email"].Value<string>(),
+                };
+
+                return user; 
+            }
+            else
+            {
+
+                throw new Exception("Invalid token");
+            }
+
+        }
+        public async Task<ZoomMeeting> CreateZoomMeeting(string accessToken, ZoomMeetingAddRequest model)
+        {
+           
+            if (accessToken != null)
+            {
+                var options = new RestClientOptions("https://api.zoom.us")
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+                var request = new RestRequest("/v2/users/me/meetings", Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", $"Bearer {accessToken}");
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(model);
+      
+
+
+                RestResponse response = await client.ExecuteAsync(request);
+                var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content);
+             
+                if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    var meeting = new ZoomMeeting
+                    {
+                        StartTime = jsonResponse.start_time,
+                        JoinUrl = jsonResponse.join_url,
+                        Topic = jsonResponse.topic,
+                        MeetingCreator = jsonResponse.host_email,
+                        MeetingId = jsonResponse.id,
+                        
+                    };
+                    return meeting;
+                }
+                else
+                {
+                    throw new Exception("Failed to create Zoom meeting");
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid token"); 
+            }
+        }
+        public int Add(BaseMeeting model)
+        {
+
+            int id = 0;
+
+            string procName = "[dbo].[Meetings_Insert]";
+            _data.ExecuteNonQuery(procName,
+             inputParamMapper: delegate (SqlParameterCollection col)
+
+             {
+                 AddCommonParams(model, col);
+
+
+               
+                 SqlParameter idOut = new SqlParameter("@Id", SqlDbType.Int);
+                 idOut.Direction = ParameterDirection.Output;
+
+                 col.Add(idOut);
+
+             }, returnParameters: delegate (SqlParameterCollection returnCollection)
+             {
+                 object oId = returnCollection["@Id"].Value;
+                 int.TryParse(oId.ToString(), out id);
+
+             });
+
+            return id;
+        }
+     
+
+        public List<UserMeetingRequest> GetById(int id)
+        {
+            List<UserMeetingRequest> list = null;
+            string procName = "[dbo].[Meetings_SelectById]";
+
+            _data.ExecuteCmd(procName, delegate (SqlParameterCollection parameterCollection)
+            {
+                parameterCollection.AddWithValue("@Id", id);
+
+            }, singleRecordMapper: delegate (IDataReader reader, short set)
+            {
+                int index = 0;
+                UserMeetingRequest message = MapSingleMeeting(reader, ref index);
+
+                if (list == null)
+                {
+                    list = new List<UserMeetingRequest>();
+                }
+
+                list.Add(message);
+            });
+
+            return list;
+        }
+
+        public async Task<MeetingInformationRequest> GetMeeting(string accessToken, long meetingId)
+        {
+
+            if (accessToken != null)
+            {
+                var options = new RestClientOptions("https://api.zoom.us")
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+                var request = new RestRequest($"/v2/past_meetings/{meetingId}", Method.Get);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", $"Bearer {accessToken}");
+                RestResponse response = await client.ExecuteAsync(request);
+                JObject jsonResponse = JObject.Parse(response.Content);
+                var meeting = new MeetingInformationRequest
+                {
+                    Topic = jsonResponse["topic"].Value<string>(),
+                    UserName = jsonResponse["user_name"].Value<string>(),
+                    StartTime = jsonResponse["start_time"].Value<DateTime>(),
+                    EndTime = jsonResponse["end_time"].Value<DateTime>(),
+                    Duration = jsonResponse["duration"].Value<int>(),
+                    ParticipantsCount = jsonResponse["participants_count"].Value<int>(),
+                };
+
+                return meeting;
+            }
+            else
+            {
+
+                throw new Exception("Invalid token");
+            }
+
+        }
+
+
+        private static void AddCommonParams(BaseMeeting model, SqlParameterCollection col)
+        {
+            col.AddWithValue("@MeetingId", model.MeetingId);
+            col.AddWithValue("@MeetingCreator", model.MeetingCreator);
+            col.AddWithValue("@UserId", model.UserId);
+            col.AddWithValue("@StartTime", model.StartTime);
+        }
+
+        private static UserMeetingRequest MapSingleMeeting(IDataReader reader, ref int index)
+        {
+            UserMeetingRequest meeting = new UserMeetingRequest();
+
+            meeting.Id = reader.GetSafeInt32(index++);
+            meeting.MeetingId = reader.GetSafeInt64(index++);
+            meeting.StartTime = reader.GetDateTime(index++);
+            meeting.MeetingCreator = reader.GetSafeString(index++);
+            meeting.UserId = reader.GetSafeInt32(index++);
+            meeting.FirstName = reader.GetSafeString(index++);
+            meeting.LastName = reader.GetSafeString(index++);
+     
+            return meeting;
+        }
+    }
+}
+
+ 
